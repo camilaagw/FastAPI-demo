08470a5a8ec04bbc54c96b2df4d34a960d75c408
Camila Andrea Gonzalez Williamson <camilaagw@gmail.com>
2021-02-25 15:50:50 +0100

    Add app title and descriptions

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(
+    title="Cool API",
+    description="A cool *API* to test **FastAPI** 🚀",
+    version="0.1.0"
+)
 
 
 @app.get("/")
 def say_hello():
+    """Say hello to the *fabulous* **Team!!!**.
+    * Hallo!
+    * Salut!
+    * Hola!
+    """
     return {"message": "Hello Team! This API seems to be working"}
 
