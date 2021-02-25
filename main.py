c76c8c4d90cb9c73bb3551c8fe740bc4fe76c899
Camila Andrea Gonzalez Williamson <camilaagw@gmail.com>
2021-02-25 12:56:07 +0100

    Add endpoint to retrieve article

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ app = FastAPI(
     version="0.1.0"
 )
 
+my_article = {
+    "content": "Apple buys U.K. startup for 1 billion"
+}
+
 
 @app.get("/")
 def say_hello():
@@ -16,3 +20,7 @@ def say_hello():
     """
     return {"message": "Hello Team! This API seems to be working"}
 
+
+@app.get("/article/{article_id}")
+def retrieve_article(article_id: int):
+    return {"content": my_article["content"], "id": article_id}
